06a46d8d5d4e940b306ba759940bea447f2d9742
abdoo <abdullah.k10204@gmail.com>
2024-11-13 23:11:03 +0500

    Tailwind configured

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="w-full h-screen flex items-center justify-center text-7xl font-bolder">eSales.pk</h1>
+    </>
+  );
+}
+
+export default App;
